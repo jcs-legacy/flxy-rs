95994a9bfd8432610ab7daa7b7e98c8c064d2121
Jen-Chieh Shen <jcs090218@gmail.com>
2021-10-18 00:52:10 +0800

    1

--- src/constants.rs
+++ src/constants.rs
@@ -6,9 +6,9 @@
  * $Notice: See LICENSE.txt for modification and distribution information
  *                   Copyright © 2021 by Shen, Jen-Chieh $
  */
-pub const SEPARATOR_FACTOR: f32 = 5.0;
+pub const SEPARATOR_FACTOR: f32 = 10.0;
 pub const SEPARATOR_REDUCE: f32 = 0.5;
-pub const CLASS_FACTOR: f32 = 3.0;
+pub const CLASS_FACTOR: f32 = 10.0;
 pub const FIRST_FACTOR: f32 = 3.0;
 pub const CLASS_REDUCE: f32 = 0.5;
 
