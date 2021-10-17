8e87fd7506dcfe21f8498342ca316e7d72a8e4a4
Jen-Chieh Shen <jcs090218@gmail.com>
2021-10-18 00:53:32 +0800

    0.1.12

--- src/constants.rs
+++ src/constants.rs
@@ -8,12 +8,12 @@
  */
 pub const SEPARATOR_FACTOR: f32 = 10.0;
 pub const SEPARATOR_REDUCE: f32 = 0.5;
-pub const CLASS_FACTOR: f32 = 10.0;
+pub const CLASS_FACTOR: f32 = 3.0;
 pub const FIRST_FACTOR: f32 = 3.0;
 pub const CLASS_REDUCE: f32 = 0.5;
 
 pub const DIST_WEIGHT: f32 = -10.0;
-pub const HEAT_WEIGHT: f32 = 5.0;
+pub const HEAT_WEIGHT: f32 = 10.0;
 pub const FACTOR_REDUCE: f32 = 0.02;
 
 pub const MAX_LEN: usize = 80;
